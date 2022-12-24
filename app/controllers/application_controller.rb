ae1243a141a73ff4d051b3bce8cf8ff5bb32875a
ayakuma309 <ayako.30901@gmail.com>
2022-12-24 19:10:16 +0900

    add: ログイン制御

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  def not_authenticated
+    flash[:warning] = t('defaults.message.require_login')
+    redirect_to login_path
+	end
 end
