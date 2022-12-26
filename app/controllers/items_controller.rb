46451c2b4cbfa2876670b12589fe3e024ee7a28d
ayakuma309 <ayako.30901@gmail.com>
2022-12-26 12:26:25 +0900

    add: 検索できるようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,34 @@
 class ItemsController < ApplicationController
   def index
-    @items = RakutenWebService::Ichiba::Item.search(keyword: '介護食')
+    create_rakuten_data(params)
+    @q = Item.ransack(params[:q])
+    @items = @q.result.page(params[:page])
   end
+
+
+  private
+    def create_rakuten_data(params)
+      @results = RakutenWebService::Ichiba::Item.search(
+        keyword: '介護食',
+        page: params[:page],
+        hits: 10)
+
+      @results.each do |result|
+        item = Item.new(read(result))
+        unless Item.all.exists?(name: item.name)
+          item.save
+        end
+      end
+    end
+
+    def read(result)
+      image= result['mediumImageUrls'][0]
+      name = result['itemName']
+      url = result['itemUrl']
+      {
+        image: image,
+        name: name,
+        url: url
+      }
+    end
 end
